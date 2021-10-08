9f180fac079b80223be34e05565a3130c18287a8
opti21 <40129778+opti21@users.noreply.github.com>
2021-10-07 23:51:29 -0500

    fixed pusher client being recreated

--- pages/api/createTokenRequest.js
+++
@@ -1,9 +0,0 @@
-import Ably from "ably/promises";
-
-export default async function handler(req, res) {
-  const client = new Ably.Realtime(process.env.ABLY_KEY);
-  const tokenRequestData = await client.auth.createTokenRequest({
-    clientId: "vibey-client",
-  });
-  res.status(200).json(tokenRequestData);
-}
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,34 +1,45 @@
 import { useState, useEffect } from "react";
 import Pusher from "pusher-js";
+import Container from "react-bootstrap/Container";
+import Card from "react-bootstrap/Card";
 
 export default function Home() {
   const [requests, setRequests] = useState([]);
 
   useEffect(() => {
-    Pusher.logToConsole = true;
-
     const pusher = new Pusher("94254303a6a5048bf191", {
       cluster: "us2",
     });
 
+    const addRequest = (newRequest) =>
+      setRequests((prevReqs) => [newRequest, ...prevReqs]);
+
     const channel = pusher.subscribe("requests");
     channel.bind("new-request", function (data) {
-      setRequests([data]);
+      addRequest(data);
+      console.log(data);
     });
-  });
+  }, []);
 
   return (
     <>
-      <h1>Janky Vibey</h1>
-      <div id="request-container">
-        {requests.map((request) => {
-          return (
-            <div>
-              {request.requester} requested {request.message}
-            </div>
-          );
-        })}
-      </div>
+      <Container>
+        <h1>Janky Vibey</h1>
+        <div id="request-container">
+          {requests.map((request, index) => {
+            return (
+              <Card key={index} style={{ margin: "30px" }}>
+                <Card.Header style={{ fontWeight: "bold" }}>
+                  {request.message}
+                </Card.Header>
+                <div id="card-text" style={{ padding: "10px" }}>
+                  {request.requester}
+                </div>
+              </Card>
+            );
+          })}
+        </div>
+      </Container>
     </>
   );
 }
